1290c373cfc9cf4de2c127aabe73d4bfd935613c
Sébastien De Pauw <sebastien.depauw@student.hogent.be>
2020-12-22 21:25:49 +0100

    Changes to models and first viewController and view

--- Essentials/models/Survey.swift
+++ Essentials/models/Survey.swift
@@ -7,23 +7,23 @@
 
 import Foundation
 
-class Survey: Codable{
-    let id: Int
-    var questions: [Question]
-    var feedback: Question
-    var roadMapItemId: Int
-    var roadMapItem: RoadMapItem?
+struct Survey: Codable{
+    public let id: Int
+    public let questions: [Question]
+    public let feedback: Question
+    public let roadMapItemId: Int
+    public let roadMapItem: RoadMapItem?
     
-    public init?(id: Int, questions: [Question], feedback: Question, roadMapItemId: Int, roadMapItem: RoadMapItem?) {
-        if id < 0 || roadMapItemId < 0 || roadMapItem == nil {
-            return nil
-        }
-        self.id = id
-        self.questions = questions
-        self.feedback = feedback
-        self.roadMapItemId = roadMapItemId
-        self.roadMapItem = roadMapItem
-    }
+//    public init?(id: Int, questions: [Question], feedback: Question, roadMapItemId: Int, roadMapItem: RoadMapItem?) {
+//        if id < 0 || roadMapItemId < 0 || roadMapItem == nil {
+//            return nil
+//        }
+//        self.id = id
+//        self.questions = questions
+//        self.feedback = feedback
+//        self.roadMapItemId = roadMapItemId
+//        self.roadMapItem = roadMapItem
+//    }
     
     public enum CodingKeys: String, CodingKey {
         case id
---
+++ Essentials/viewController/ChangeInitiativeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ChangeInitiativeController.swift
+//  Essentials
+//
+//  Created by Sébastien De Pauw on 22/12/2020.
+//
+
+import UIKit
+
+class ChangeInitiativeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Changeinitiative"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
