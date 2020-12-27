4da982014ffdcfe76b355ba670612a144d1445e7
Sébastien De Pauw <sebastien.depauw@student.hogent.be>
2020-12-27 23:31:56 +0100

    Starter files and layout to Survey screen

---
+++ Essentials/viewController/SurveyDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SurveyDetailsViewController.swift
+//  Essentials
+//
+//  Created by Sébastien De Pauw on 27/12/2020.
+//
+
+import UIKit
+
+class SurveyDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Survey detail"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
