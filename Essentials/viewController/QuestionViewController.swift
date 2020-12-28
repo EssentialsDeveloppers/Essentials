3d3e5cadcef0117031b7012bfb95e24300b2dfa9
Sébastien De Pauw <sebastien.depauw@student.hogent.be>
2020-12-28 21:24:40 +0100

    initial branch commit

--- Essentials/viewController/QuestionViewController.swift
+++ Essentials/viewController/QuestionViewController.swift
@@ -11,7 +11,7 @@ class QuestionViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Survey detail"
+        title = "Survey"
 
         // Do any additional setup after loading the view.
     }
