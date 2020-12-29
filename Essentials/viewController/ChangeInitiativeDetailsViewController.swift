df8706c1086dbeda012d36c0060837ff36d49830
Kilian Hoefman <kilian.hoefman@student.hogent.be>
2020-12-29 15:04:00 +0100

    start of second viewcontroller + dateformatting integrated

--- Essentials/viewController/ChangeInitiativeDetailsViewController.swift
+++ Essentials/viewController/ChangeInitiativeDetailsViewController.swift
@@ -12,11 +12,36 @@ class ChangeInitiativeDetailsViewController : UIViewController {
     
     var changeInitiative : ChangeInitiative?
     
+    @IBOutlet weak var titleChangeInitiative: UILabel!
+    @IBOutlet weak var descriptionText: UILabel!
+    @IBOutlet weak var startDateTime: UILabel!
+    @IBOutlet weak var endDateTime: UILabel!
+    @IBOutlet weak var progress: UIProgressView!
+    @IBOutlet weak var addAlarmButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(changeInitiative?.changeType?.id ?? "oops")
-        print(changeInitiative?.changeSponsor?.firstName ?? "oops")
-        print(changeInitiative?.description ?? "oops")
-        print(changeInitiative?.progress ?? "oops")
+        initView()
+    }
+    
+    private func initView() {
+        guard let data = changeInitiative else { return }
+        titleChangeInitiative.text = data.title
+        descriptionText.text = data.description
+        startDateTime.text = formatDateTime(date: data.startDate)
+        endDateTime.text = formatDateTime(date: data.endDate)
+        progress.progress = Float(data.progress)
+    }
+    
+    
+    private func formatDateTime(date: String) -> String {
+        let initialDateFormatter = DateFormatter()
+        initialDateFormatter.dateFormat = "yyyy-mm-dd'T'HH:mm:ss.SSSSSSS"
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "E, d MMM yyyy HH:mm"
+        
+        let date: Date? = initialDateFormatter.date(from: date)
+        return dateFormatter.string(from: date!)
     }
 }
