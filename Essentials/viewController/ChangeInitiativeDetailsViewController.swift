6ff964cca149c211263902ad48f519d1b66fc0b3
Kilian Hoefman <kilian.hoefman@student.hogent.be>
2021-01-09 18:00:56 +0100

    Added some explanation to each function

--- Essentials/viewController/ChangeInitiativeDetailsViewController.swift
+++ Essentials/viewController/ChangeInitiativeDetailsViewController.swift
@@ -26,6 +26,10 @@ class ChangeInitiativeDetailsViewController : UIViewController, MFMailComposeVie
         initView()
     }
     
+    /**
+     Method for initializing the view and filling all labels
+     - Author: Kilian Hoefman
+     */
     private func initView() {
         guard let data = changeInitiative else { return }
         //Change Initiative
@@ -42,6 +46,13 @@ class ChangeInitiativeDetailsViewController : UIViewController, MFMailComposeVie
 
     }
     
+    /**
+     Extra functionality: method for sending an email to a change sponsor when problems or questions arise. Used MFMailComposeViewController to open the iOS mail application
+     
+     URL: https://developer.apple.com/documentation/messageui/mfmailcomposeviewcontroller
+     
+     - Author: Kilian Hoefman
+     */
     @IBAction func sendMailToChangeManager(_ sender: UIButton!){
         let emailChangeManager = changeInitiative!.changeSponsor!.email
         let subject = "Questions about " + changeInitiative!.title + " initiative"
@@ -56,7 +67,7 @@ class ChangeInitiativeDetailsViewController : UIViewController, MFMailComposeVie
 
             present(mail, animated: true)
         }else{
-            let alert = UIAlertController(title: "Unavailable email", message: "The email service is currently unavailable on your device", preferredStyle: UIAlertController.Style.alert)
+            let alert = UIAlertController(title: "Email unavailable", message: "The email service is currently unavailable on your device", preferredStyle: UIAlertController.Style.alert)
             let alertAction = UIAlertAction(title: "Dismiss", style: UIAlertAction.Style.default)
                     {
                         (UIAlertAction) -> Void in
@@ -66,17 +77,23 @@ class ChangeInitiativeDetailsViewController : UIViewController, MFMailComposeVie
                     {
                         () -> Void in
                     }
-            
         }
     }
+    
+    /**
+     Function to close MFMailComposeView when the user decides to cancel the email.
+     User gets redirected to the detailscreen
+     - Author: Kilian Hoefman
+     */
     
     func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
                 controller.dismiss(animated: true)
             }
     
     
-    /*
-     Time formatter
+    /**
+     Method for formatting datetime coming from the API. Displayed the date as Day, number month year hour:minutes
+     - Author: Kilian Hoefman
      */
     private func formatDateTime(date: String) -> String {
         let initialDateFormatter = DateFormatter()
--- Essentials/viewController/ChangeInitiativeViewController.swift
+++ Essentials/viewController/ChangeInitiativeViewController.swift
@@ -10,19 +10,34 @@ import Alamofire
 
 class ChangeInitiativeViewController: UITableViewController {
 
+    /// List of changeInitiatives that gets loaded by the API
     var changeInitiatives: [ChangeInitiative] = []
     
+    /// Selected Change Initiative used in Segue
     var selectedChangeInit: ChangeInitiative?
     
+    /**
+     - Author: Kilian Hoefman
+     */
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchChangeInitiatives()
     }
     
+    /**
+     Override function for tableView & number of rows
+     - Author: Kilian Hoefman
+     - returns: The amount of table cells in the Table View
+     */
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return changeInitiatives.count
     }
     
+    /**
+     Override function for tableView & index
+     - Author: Kilian Hoefman
+     - returns: The specific cell at the given index
+     */
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChangeInitCell", for: indexPath)
         let changeInitiative = changeInitiatives[indexPath.row]
@@ -30,11 +45,20 @@ class ChangeInitiativeViewController: UITableViewController {
         return cell
     }
     
+    /**
+     Override function for tableview & selected cell
+     - Author: Kilian Hoefman
+     - returns: The index of the selected cell
+     */
     override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
         selectedChangeInit = changeInitiatives[indexPath.row]
         return indexPath
     }
     
+    /**
+     Override method to pass the Selected Change Initiative to the details View Controller via a segue
+     - Author: Kilian Hoefman
+     */
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let detailsOfChangeInit = segue.destination as? ChangeInitiativeDetailsViewController else {
                 return
@@ -45,7 +69,17 @@ class ChangeInitiativeViewController: UITableViewController {
 
 }
 
+//MARK: - ALAMOFIRE API CALL
+
+/**
+Extension of ChangeInitiativeViewController for API call
+ */
 extension ChangeInitiativeViewController {
+    /**
+     Function for fetching the Change Initiatives coming from the API
+     - Author: Kilian Hoefman
+     Fills the ChangeInitiatives list with data from the API
+     */
     func fetchChangeInitiatives(){
         AF.request("https://essentialsapi-forios.azurewebsites.net/api/ChangeInitiatives/GetChangeInitiativesForEmployee/\(Globals.isChangeManager ? Globals.changeManager!.id : Globals.employee!.id)")
             .validate().responseDecodable(of: [ChangeInitiative].self) {
