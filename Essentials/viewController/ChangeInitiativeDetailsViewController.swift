8f77285a4c65a1da1495e99dbac27daf891463d0
Kilian Hoefman <kilian.hoefman@student.hogent.be>
2021-01-09 15:25:49 +0100

    Extra features added to change initiative detail screen, mail functionality added

--- Essentials/viewController/ChangeInitiativeDetailsViewController.swift
+++ Essentials/viewController/ChangeInitiativeDetailsViewController.swift
@@ -7,8 +7,9 @@
 
 import Foundation
 import UIKit
+import MessageUI
 
-class ChangeInitiativeDetailsViewController : UIViewController {
+class ChangeInitiativeDetailsViewController : UIViewController, MFMailComposeViewControllerDelegate {
     
     var changeInitiative : ChangeInitiative?
     
@@ -17,7 +18,10 @@ class ChangeInitiativeDetailsViewController : UIViewController {
     @IBOutlet weak var startDateTime: UILabel!
     @IBOutlet weak var endDateTime: UILabel!
     @IBOutlet weak var progress: UIProgressView!
-    @IBOutlet weak var addAlarmButton: UIButton!
+    @IBOutlet weak var progressPercentage: UILabel!
+    @IBOutlet weak var changeManagerName: UILabel!
+    @IBOutlet weak var emailChangeManager: UILabel!
+    @IBOutlet weak var mailToChangeManager: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,14 +30,57 @@ class ChangeInitiativeDetailsViewController : UIViewController {
     
     private func initView() {
         guard let data = changeInitiative else { return }
+        
+        //Change Initiative
         titleChangeInitiative.text = data.title
         descriptionText.text = data.description
         startDateTime.text = formatDateTime(date: data.startDate)
         endDateTime.text = formatDateTime(date: data.endDate)
         progress.progress = Float(data.progress)
+        progressPercentage.text = String(format: "%d %%", data.progress)
+                
+        //Change Manager
+        changeManagerName.text = data.changeSponsor!.firstName + " " + data.changeSponsor!.lastName
+        emailChangeManager.text = data.changeSponsor!.email
+
     }
     
+    @IBAction func sendMailToChangeManager(_ sender: UIButton!){
+        let emailChangeManager = changeInitiative!.changeSponsor!.email
+        let subject = "Questions about " + changeInitiative!.title + " initiative"
+        let body = ""
+
+        if MFMailComposeViewController.canSendMail() {
+            let mail = MFMailComposeViewController()
+            mail.mailComposeDelegate = self
+            mail.setToRecipients([emailChangeManager])
+            mail.setSubject(subject)
+            mail.setMessageBody(body, isHTML: false)
+
+            present(mail, animated: true)
+        }else{
+            let alert = UIAlertController(title: "Unavailable email", message: "The email service is currently unavailable on your device", preferredStyle: UIAlertController.Style.alert)
+            let alertAction = UIAlertAction(title: "Dismiss", style: UIAlertAction.Style.default)
+                    {
+                        (UIAlertAction) -> Void in
+                    }
+                    alert.addAction(alertAction)
+                    present(alert, animated: true)
+                    {
+                        () -> Void in
+                    }
+            
+        }
+    }
     
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+                controller.dismiss(animated: true)
+            }
+    
+    
+    /*
+     Time formatter
+     */
     private func formatDateTime(date: String) -> String {
         let initialDateFormatter = DateFormatter()
         initialDateFormatter.dateFormat = "yyyy-mm-dd'T'HH:mm:ss.SSSSSSS"
